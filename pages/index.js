2d7c71d0b9388fa06d37ec16b07114535bc96c18
BeaReis <pcreis.beatriz@gmail.com>
2022-04-22 21:32:24 -0300

    Added Title (Activity) and Subtitle (Caetgory)

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import Clock from "../src/components/clock/clock";
 import Setup from "../src/components/setup/setup";
 import Mode from "../src/components/mode/mode";
-import { Wrapper, InnerWrapper } from "../src/components/common/style";
+import { Wrapper, InnerWrapper, Title } from "../src/components/common/style";
+import Activity from "../src/components/activity/activity";
 
 function Index() {
   /*  */
@@ -10,18 +11,25 @@ function Index() {
   const [shortBreak, setShortBreak] = useState(5);
   const [longBreak, setLongBreak] = useState(15);
   const [mode, setMode] = useState("pomodoro");
+  const [activity, setActivity] = useState("Welcome to your pomodoro!");
+  const [category, setCategory] = useState("Category");
+  const [status, setStatus] = useState(false);
 
   return (
     <>
       <Wrapper>
         <InnerWrapper>
+          <Title>{activity}</Title>
+          <Title category>{category}</Title>
           <Mode setMode={setMode} />
           <Clock
             mode={mode}
             pomodoro={pomodoro}
             shortBreak={shortBreak}
             longBreak={longBreak}
+            status={status}
           />
+          <Activity setActivity={setActivity} setCategory={setCategory}/>
         </InnerWrapper>
         <Setup
           setPomodoro={setPomodoro}
