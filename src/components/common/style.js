bf3c8ad38fcd7a813659e9fa9dcb8415e70ee058
BeaReis <pcreis.beatriz@gmail.com>
2022-04-26 20:54:12 -0300

    Added button for settings visibility and added adjustments to layout

--- src/components/common/style.js
+++ src/components/common/style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import scheme from '../../../public/colors.json';
 
 export const Body = styled.div`
     display: flex;
@@ -11,27 +12,50 @@ export const Wrapper = styled.div`
     flex-direction: row;
     @media (max-width: 720px) {
         flex-direction: column;
-        padding: 10px;
+        margin-bottom: 100px;
     }
     justify-content: center;
     align-items: center;
     width: 100%;
-    height: 100%;
     padding: 20px;
+    margin: 30px;
 `;
 
 export const InnerWrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    margin-right: 50px;
+    width: 350px;
+    @media (max-width: 720px) {
+        margin: 0;
+    }
 `;
 
 export const Title = styled.p`
-    font-size: ${props => props.category ? '16px' : '32px'};
+    font-size: ${props => props.category ? '16px' : '30px'};
     text-align: center;
     color: white;
+    word-break: break-word;
     margin: ${props => props.category ? '10px' : '0 15px 0 15px'};
     @media (max-width: 720px){
         font-size: ${props => props.category ? '16px' : '24px'};
+    }
+`;
+
+export const Button = styled.button`
+    position: fixed;
+    right: 10px;
+    margin: 10px;
+    border-radius: 50%;
+    background-color: ${scheme.colors.primary[100]};
+    width: 50px;
+    height: 50px;
+    @media (max-width: 720px) {
+        bottom: 70px;
+        z-index: 10;
+    }
+    @media (min-width: 720px) {
+        top: 65px;
     }
 `;
