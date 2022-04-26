7114afd053c7c6207c4f726be2472e38b2ab9f0c
BeaReis <pcreis.beatriz@gmail.com>
2022-04-26 20:52:16 -0300

    Added Title changes on Enter Keypress

--- src/components/activity/activity.jsx
+++ src/components/activity/activity.jsx
@@ -9,22 +9,40 @@ function Activity(props) {
     <>
       <Wrapper>
         <Category
+          maxLength={30}
           placeholder="Add category..."
           value={category}
           onChange={(event) => {
             const value = event.target.value;
             setCategory(value);
+
+          }}
+          onKeyDown={(event) => {
+            if(event.key === "Enter") {
+              event.preventDefault;
+              props.setActivity(activity);
+              props.setCategory(category);
+            }
           }}
         />
         <Description
+          maxLength={40}
           placeholder="Add activity..."
           value={activity}
           onChange={(event) => {
             const value = event.target.value;
             setActivity(value);
           }}
+          onKeyDown={(event) => {
+            if(event.key === "Enter") {
+              event.preventDefault;
+              props.setActivity(activity);
+              props.setCategory(category);
+            }
+          }}
         />
         <AddButton
+         
           onClick={() => {
             props.setActivity(activity);
             props.setCategory(category);
